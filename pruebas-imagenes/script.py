507fc515221bf0320a7d50c62009433672b1f9eb
wac-Andrea <andrea.bombin@weareclickers.com>
2024-11-27 12:14:26 +0100

    Correcto funcionamiento. Añadido system prompt a user prompt

--- pruebas-imagenes/script.py
+++ pruebas-imagenes/script.py
@@ -1,41 +1,58 @@
-import base64
 from openai import OpenAI
-import os 
+import base64
+import requests
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-client = OpenAI(
-    api_key = os.getenv("OPENAI_API_KEY")
-)
+api_key = os.getenv("OPENAI_API_KEY")
 
-SYSTEM_PROMPT="""Tienes que analizar la imagen y considerar el prompt del usuario""".strip()
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
 
-def image_prompt(user_prompt, encoded_image):
-    response=client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {
-            "role": "system",
-            "content": SYSTEM_PROMPT,
-            },
-            {
-            "role": "user", 
-            "content": user_prompt + encoded_image
-            }
-        ],
-        max_tokens=40000,    
-)
-    print(response.choices[0])
-    #print(response.usage)
-    return response.choices[0].message.content
+image_path = "../images/pierna.webp"
 
- 
-def encode_image_to_base64(image_path):
-    with open(image_path, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode('utf-8')
+base64_image = encode_image(image_path)
 
-image_path = "../images/fine.webp"
+SYSTEM_PROMPT = """Eres una persona detallista y observadora. 
+Cuando ves una imagen, te fijas en todos los detalles que contiene y sabes describirla a la perfección para
+que otras personas entiendan lo que estás viendo. Además, tienes conocimientos en medicina y veterinaria
+por lo que se te hace muy fácil analizar una imagen (por ejemplo, una radiografía o la foto de una herida)
+y saber qué le pasa al paciente. Sé claro y acertado en tus diagnósticos y respuestas."""
 
-MyEncodedImage = encode_image_to_base64(image_path)
-print(f"Size in bytes of the image is : {MyEncodedImage.__sizeof__()/1024:2.2f}")
+headers = {
+  "Content-Type": "application/json",
+  "Authorization": f"Bearer {api_key}"
+}
+
+payload = {
+  "model": "gpt-4o",
+  "messages": [
+    {
+        "role": "system",
+        "content": SYSTEM_PROMPT
+    },
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "text",
+          "text": "Este paciente se queja de dolores de cabeza. ¿Te da la radiografía una pista de qué le ocurre?"
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": f"data:image/jpeg;base64,{base64_image}"
+          }
+        }
+      ]
+    }
+  ],
+  "max_tokens": 3000
+}
+
+response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+print(response.json()['choices'][0]['message']['content'])
